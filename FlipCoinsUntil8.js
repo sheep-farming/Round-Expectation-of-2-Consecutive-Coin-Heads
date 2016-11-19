d57bcbbf1d83feaf31b4666862a889a302927729
DuPupu <zouluoyi@gmail.com>
2016-11-19 21:25:59 +0800

    Answer is 7

--- FlipCoinsUntil8.js
+++ FlipCoinsUntil8.js
@@ -1,19 +1,16 @@
-function flip(){
-    return Math.random()>=0.5?1:0;
-}
 function t(){
     var seq = [];
     var cnt = 0;
     var flag = false;
-    while (flag==false){
-        cnt++;
-        seq.push(flip());
-        if(seq.length>2){
-        if((seq[seq.length-2]==1)&(seq[seq.length-1]==1)){return cnt;}}
+    while (seq.push(Math.random()>=0.5?1:0),cnt++,flag==false){
+        if(seq.length>2 &
+            (seq[seq.length-2]==1) &
+            (seq[seq.length-1]==1)
+        )return cnt;
     }
 }
 var cntList=[];
-for (var i=0;i<9999999;i++){
+for (var i=0;i<142857;i++){
     cntList.push(t());
 }
 sum = cntList.reduce(
@@ -21,4 +18,4 @@ sum = cntList.reduce(
         return a+b;
     }, 
     0);
-console.log(sum/9999999);
+console.log(sum/142857);
